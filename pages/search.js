d479590a20ee7b338999ccfe98863840288ff44c
Aldry <aldryv@gmail.com>
2023-05-18 03:20:40 -0300

    merged search results component directly inside search page

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,10 @@
 import { useRouter } from 'next/router';
 import { getSortedPostsData } from '../lib/posts';
-import SearchResults from './components/SearchResults';
 import Layout, { siteTitle } from '../components/layout';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import * as S from '../styles/pages/search.js';
 
 export default function Search({ allPostsData }) {
   const router = useRouter();
@@ -16,7 +19,26 @@ export default function Search({ allPostsData }) {
 
   return (
     <Layout search>
-      <SearchResults searchResults={filteredPosts} />
+      <div searchResults={filteredPosts}>
+        <S.SearchResult>
+          <h1>Resultados da sua pesquisa:</h1>
+          <button onClick={() => router.back()}>Voltar</button>
+          {filteredPosts.map((post) => (
+            <>
+              <div className="all-posts" key={post.id}>
+                <Link className="post-link" href={`/posts/${post.id}`}>
+                  <Image
+                    src={post.banner}
+                    height={186}
+                    width={280}
+                  />
+                  <h3 className="post-title">{post.title}</h3>
+                </Link>
+              </div>
+            </>
+          ))}
+        </S.SearchResult>
+      </div>
     </Layout>
   )
 }
