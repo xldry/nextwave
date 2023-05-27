8a443e1ee4cb4069edb2fed85d3d0b250f5fd983
Aldry <aldryv@gmail.com>
2023-05-27 20:30:30 -0300

    last.fm api call serverless function

--- components/RecentLikes/index.js
+++ components/RecentLikes/index.js
@@ -8,12 +8,9 @@ const RecentLikes = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-        `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=jumiknight&api_key=${process.env.NEXT_PUBLIC_LASTFM_API_KEY}&format=json&period=1month&limit=6`
-        );
-        const { topalbums } = response.data;
-        const { album } = topalbums;
-        setRecentArtists(album);
+        const response = await axios.get('/api/getTopAlbums');
+        const { recentArtists } = response.data;
+        setRecentArtists(recentArtists);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
