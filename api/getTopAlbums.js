34509a58c6f5ac742426cb30af752581369c9471
Aldry <aldryv@gmail.com>
2023-05-27 20:23:22 -0300

    switched axios for native fetch call

--- api/getTopAlbums.js
+++ api/getTopAlbums.js
@@ -1,15 +1,19 @@
-import axios from 'axios';
-
 export default async (req, res) => {
   try {
-    const response = await axios.get(
-      `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=jumiknight&api_key=${process.env.NEXT_PUBLIC_LASTFM_API_KEY}&format=json&period=1month&limit=6`
-    );
-    const { topalbums } = response.data;
+    const url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=jumiknight&api_key=${process.env.NEXT_PUBLIC_LASTFM_API_KEY}&format=json&period=1month&limit=6`;
+    
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Error fetching data from Last.fm API');
+    }
+    
+    const data = await response.json();
+    const { topalbums } = data;
     const { album } = topalbums;
+    
     res.status(200).json({ recentArtists: album });
   } catch (error) {
     console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Error fetching data' });
+    res.status(500).json({ error: 'Error fetching data from Last.fm API' });
   }
 };
