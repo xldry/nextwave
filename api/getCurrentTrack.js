128caa41cfe3239136081c22a13c33599e4fb3e3
Aldry Vinicius <aldryv@gmail.com>
2023-06-21 15:02:34 -0300

    fix server side api call

--- api/getCurrentTrack.js
+++ api/getCurrentTrack.js
@@ -1,19 +1,28 @@
-export default async (req, res) => {
+export default async function handler(req, res) {
   try {
-    const url = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jumiknight&api_key=${process.env.NEXT_PUBLIC_LASTFM_API_KEY}format=json&limit=1`;
-    
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Error fetching data from Last.fm API');
-    }
-    
-    const data = await response.json();
-    const { recenttrack } = data;
-    const { track } = recenttrack;
+    // Replace this with your logic to fetch the current track from Last.fm API
+    const track = await fetchCurrentTrack();
 
-    res.status(200).json({ recentTracks: track });
+    res.status(200).json({ currentTrack: track });
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Error fetching data from Last.fm API' });
+    console.error('Error fetching current track:', error);
+    res.status(500).json({ error: 'Error fetching current track' });
   }
-};
+}
+
+// Example function to fetch the current track from Last.fm API
+async function fetchCurrentTrack() {
+  const url = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jumiknight&api_key=${process.env.NEXT_PUBLIC_LASTFM_API_KEY}format=json&limit=1`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Error fetching data from Last.fm API');
+  }
+
+  const data = await response.json();
+  const { recenttracks } = data;
+  const { track } = recenttracks;
+  
+  // Return the current track information
+  return track;
+}
